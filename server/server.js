695ae5428440f3d40f091370072c84262294ae8f
FransLarson <franslarson123@gmail.com>
2020-12-17 19:14:35 -0500

    Getting ready for NGINX

--- server/server.js
+++ server/server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(morgan('dev'));
 app.use(express.static(PUBLIC_DIR));
 
+app.get('/loaderio-96de71fcf2e638db6194fc0d16fd5d3f.txt', function(req, res){
+  res.sendFile(__dirname + '/loaderio-96de71fcf2e638db6194fc0d16fd5d3f.txt');
+});
+
 // Handling asset requests for webpack bundles by passing off requests to the bundles router
 app.use('/bundles', router.bundles);
 // Handling AJAX requests to the API by passing off requests to the api router
